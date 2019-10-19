5063a51cac73fe8471577387104c0536bbdc36cd
Edwin <Edwin.Martinez@people10.com>
2019-10-19 10:08:20 -0400

    Added cartIcon and cardDropdown componente into our application. Implemented cardReducer

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -5,7 +5,7 @@ import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => (
-  <div className="cart-drodown">
+  <div className="cart-dropdown">
     <div className="cart-items">
 
     </div>
