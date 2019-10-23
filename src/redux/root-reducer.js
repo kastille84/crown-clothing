979f75c0d7e640fa80ac07b4e9d31cc75c78ea2e
Edwin <Edwin.Martinez@people10.com>
2019-10-23 12:51:44 -0400

    Added collection Overview component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,29 @@
 import {combineReducers}  from 'redux';
+import {persistReducer} from 'redux-persist';
+// storage is actually our window's localStorage
+import storage from 'redux-persist/lib/storage';
+
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop//shop.reducer';
 
-export default combineReducers({
+//define a new persist config
+const persistConfig = {
+  //key, at what point in our reduce obj do we want to start storing
+  key: 'root',
+  storage: storage,
+  //array containing str name of any reducer we want to store
+  whitelist: ['cart']
+
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
+import {persistStore} from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
@@ -9,4 +10,7 @@ const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+//it's a persisted version of our store
+const persistor = persistStore(store);
+
+export default {store, persistor};
