7d29c97629ca37763ec3193f637fa9b543cf56a7
Edwin <Edwin.Martinez@people10.com>
2019-10-24 18:28:16 -0400

    Updating Stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const onToken = token => {
+  //we are just console.loging because we wont make actual payments right now
+  console.log(token)
+  alert('Payment Successfule')
+}
+
+const StripeCheckoutButton = ({price}) => {
+  //stripe wants price in cents, for proper charge to be made
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_getYourOwwKey";
+
+  return (
+    //go to github repo to see all the optional props you can pass
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing"
+      billingAddress
+      shippingAddress
+      imaage='https://svgshare.com/i/CUz.svg'
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      //token, is a success callback func that triggers when we submit
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+
+}
+
+//we imported it in checkout component
+export default StripeCheckoutButton;
